bdc168c34d38950a3e724b5da0f36087608e1943
Fazıl <azimligil62@gmail.com>
2026-03-22 00:51:56 +0300

    port fix

--- server.js
+++ server.js
@@ -58,7 +58,8 @@ const server = http.createServer(async (req, res) => {
 (async () => {
     await baglan();
 
-    server.listen(3000, () => {
-        console.log("Server çalıştı: http://localhost:3000");
-    });
+    const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Server çalıştı: " + PORT);
+});
 })();
